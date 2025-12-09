feeb7a7c427977afef16dea8bae6589f61c7f577
John Rey Cutab <96228393+JohnRey7@users.noreply.github.com>
2025-12-10 00:45:46 +0800

    Actions

--- frontend/nas-system/components/admin/UserList.tsx
+++ frontend/nas-system/components/admin/UserList.tsx
@@ -315,7 +315,7 @@ export function UserList() {
               <TableHead>Email</TableHead>
               <TableHead>Role</TableHead>
               <TableHead>Status</TableHead>
-              <TableHead className="text-center translate-x-[0px]">Actions</TableHead>
+              <TableHead className="text-center">Actions</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
